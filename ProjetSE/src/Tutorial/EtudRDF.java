2ca07a0ecdf68e2e3d22759954d746aaa6ddc34e
Jaf270 <thomas.losbar@gmail.com>
2014-10-22 09:09:01 +0200

    ProjetSE

---
+++ ProjetSE/src/Tutorial/EtudRDF.java
@@ -0,0 +1,10 @@
+package Tutorial;
+
+public class EtudRDF {
+	//private String etudNS ="file:/home/olive/_mesCours/kr/etudIns2.rdf"; 
+	
+	public static void main(String[] args) {
+		EtudRDF etudRDF= new EtudRDF();
+		etudRDF.load();
+	}
+}
