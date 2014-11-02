ab9eb6d0de6ed5de37d0ef73cee2cd9140058654
Jaf270 <thomas.losbar@gmail.com>
2014-11-02 15:39:51 +0100

    l'application web operationnelle à 68%
    très gros commit

--- ProjetSE_interface/src/controller/SearchEngineAction.java
+++ ProjetSE_interface/src/controller/SearchEngineAction.java
@@ -4,11 +4,15 @@ import javax.servlet.http.HttpServletRequest;
 
 class SearchEngineAction extends Action {
 
+	public SearchEngineAction() {
+	}
 	@Override
 	public void execute(HttpServletRequest request) {
 		// TODO Partie de VINCENT 
 		// Recuperation du texte ecrit dans le formulaire de recherche
 		// et lancement d'une recherche interne puis patati patata
+		
+		
 		
 	}
 
