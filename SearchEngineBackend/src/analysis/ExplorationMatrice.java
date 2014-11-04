6dead32212f3b2bca844763e6967b0687918d728
Jaf270 <thomas.losbar@gmail.com>
2014-11-04 10:21:16 +0100

    souci lib ?

--- SearchEngineBackend/src/analysis/ExplorationMatrice.java
+++ SearchEngineBackend/src/analysis/ExplorationMatrice.java
@@ -66,8 +66,8 @@ public class ExplorationMatrice {
 
 			// remplissage de notre liste de noeud
 			JSONObject aNode = new JSONObject();
-			/*aNode.putIfAbsent("name", noeud1);
-		 	aNode.putIfAbsent("theme", new Integer(0));*/
+			aNode.putIfAbsent("name", noeud1);
+		 	aNode.putIfAbsent("theme", new Integer(0));
 			if (!listNodes.contains(aNode)) {
 				listNodes.add(aNode);
 			}
